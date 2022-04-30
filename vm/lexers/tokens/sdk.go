d6bf93ed50b312d2da60d5df8b5bab65f2311ef1
Steve Rodrigue <stev.rodr@gmail.com>
2022-04-29 21:36:24 -0400

    Added the token interfaces

--- vm/lexers/tokens/sdk.go
+++ vm/lexers/tokens/sdk.go
@@ -2,9 +2,23 @@ package tokens
 
 import "github.com/steve-care-software/stevecare/vm/lexers/cardinality"
 
+// Builder represents the token builder
+type Builder interface {
+	Create() Builder
+	WithLList(lines Lines) Builder
+	Now() (Token, error)
+}
+
 // Token represents a token
 type Token interface {
 	Lines() Lines
+}
+
+// LinesBuilder represents the lines builder
+type LinesBuilder interface {
+	Create() LinesBuilder
+	WithList(lines []Line) LinesBuilder
+	Now() (Lines, error)
 }
 
 // Lines represents lines
@@ -12,15 +26,38 @@ type Lines interface {
 	List() []Line
 }
 
+// LineBuilder represents the line builder
+type LineBuilder interface {
+	Create() LineBuilder
+	WithList(elements []ElementWithCardinality) LineBuilder
+	Now() (Line, error)
+}
+
 // Line represents token lines
 type Line interface {
 	List() []ElementWithCardinality
+}
+
+// ElementWithCardinalityBuilder represents the element with cardinality builder
+type ElementWithCardinalityBuilder interface {
+	Create() ElementWithCardinalityBuilder
+	WithElement(element Element) ElementWithCardinalityBuilder
+	WithCardinality(cardinality ElementWithCardinality) ElementWithCardinalityBuilder
+	Now() (ElementWithCardinality, error)
 }
 
 // ElementWithCardinality represents element with cardinality
 type ElementWithCardinality interface {
 	Element() Element
 	Cardinality() cardinality.Cardinality
+}
+
+// ElementBuilder represents the element builder
+type ElementBuilder interface {
+	Create() ElementBuilder
+	WithByte(byteValue byte) ElementBuilder
+	WithToken(token Token) ElementBuilder
+	Now() (Element, error)
 }
 
 // Element represents a token element
