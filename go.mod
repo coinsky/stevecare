a8e5cceeae396934a36b86cc9d1251a985d1863b
Steve Rodrigue <stev.rodr@gmail.com>
2022-04-29 21:14:56 -0400

    Added the cardinality interfaces

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/steve-care-software/stevecare
+
+go 1.16
---
+++ vm/lexers/cardinality/sdk.go
@@ -0,0 +1,32 @@
+package cardinality
+
+// Builder represents the cardinality builder
+type Builder interface {
+	Create() Builder
+	WithRange(rnge Range) Builder
+	WithSpecific(specific uint) Builder
+	Now() (Cardinality, error)
+}
+
+// Cardinality represents the cardinality
+type Cardinality interface {
+	IsRange() bool
+	Range() Range
+	IsSpecific() bool
+	Specific() uint
+}
+
+// RangeBuilder represents the range builder
+type RangeBuilder interface {
+	Create() RangeBuilder
+	WithMinimum(min uint) RangeBuilder
+	WithMaximum(max uint) RangeBuilder
+	Now() (Range, error)
+}
+
+// Range represents the cardinality range
+type Range interface {
+	Min() uint
+	HasMax() bool
+	Max() uint
+}
